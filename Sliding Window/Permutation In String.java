ce10fe051124fe2835fd5e55a52b5de0bc872109
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-04-09 09:40:11 -0700

    Update Permutation In String

--- Sliding Window/Permutation In String.java
+++ Sliding Window/Permutation In String.java
@@ -1,3 +1,4 @@
+--------------------------------------------------------------------------------------JAVA------------------------------------------------------------------------------------------
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length() > s2.length())
@@ -28,3 +29,44 @@ class Solution {
         return true;
     }
 }
+--------------------------------------------------------------------------------------C#------------------------------------------------------------------------------------------
+
+    public class Solution {
+    public bool CheckInclusion(string s1, string s2) {
+        if (s1.Length > s2.Length)
+            return false;
+
+        int[] f1 = new int[26];
+        int[] f2 = new int[26];
+
+        foreach (char ch in s1) {
+            f1[ch - 'a'] += 1;
+        }
+
+        for (int i = 0; i < s1.Length; i++) {
+            f2[s2[i] - 'a'] += 1;
+        }
+
+        if (IsOk(f1, f2))
+            return true;
+
+        for (int i = s1.Length; i < s2.Length; i++) {
+            f2[s2[i] - 'a'] += 1;
+            f2[s2[i - s1.Length] - 'a'] -= 1;
+            if (IsOk(f1, f2))
+                return true;
+        }
+
+        return false;
+    }
+
+    private bool IsOk(int[] f1, int[] f2) {
+        for (int i = 0; i < f1.Length; i++) {
+            if (f1[i] != f2[i])
+                return false;
+        }
+        return true;
+    }
+}
+
+
