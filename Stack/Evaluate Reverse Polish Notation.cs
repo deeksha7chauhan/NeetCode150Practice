a1e2799cb3d0f0b3128a4ac8c93da3b7fcaeebcf
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-04-16 14:57:14 -0700

    Update Evaluate Reverse Polish Notation.cs

--- Stack/Evaluate Reverse Polish Notation.cs
+++ Stack/Evaluate Reverse Polish Notation.cs
@@ -1,1 +1,30 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        Stack<int> stacker = new Stack<int>();
+
+        foreach (string s in tokens) {
+            if (s == "+") {
+                stacker.Push(stacker.Pop() + stacker.Pop());
+            }
+            else if (s == "-") {
+                int a = stacker.Pop();
+                int b = stacker.Pop();
+                stacker.Push(b - a); // order matters
+            }
+            else if (s == "/") {
+                int a = stacker.Pop();
+                int b = stacker.Pop();
+                stacker.Push(b / a); // integer division
+            }
+            else if (s == "*") {
+                stacker.Push(stacker.Pop() * stacker.Pop());
+            }
+            else {
+                stacker.Push(int.Parse(s));
+            }
+        }
+
+        return stacker.Pop();
+    }
+}
 
