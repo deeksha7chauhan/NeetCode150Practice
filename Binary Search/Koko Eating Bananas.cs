a489de508fb81e9ec1305b986a691196ee83435e
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-04-23 13:27:27 -0700

    Update Koko Eating Bananas.cs

--- Binary Search/Koko Eating Bananas.cs
+++ Binary Search/Koko Eating Bananas.cs
@@ -1,26 +1,35 @@
 public class Solution {
-    public int MinEatingSpeed(int[] piles, int h) {
+    public int MinEatingSpeed(int[] piles, int h) 
+    {
         int l = 1;
         int r = piles.Max();
         int res = r;
 
-        while (l <= r) {
-            int k = (l + r) / 2;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2; // candidate eating speed
+            long totTime = 0;
 
-            long totalTime = 0;
-            foreach (int p in piles) {
-                totalTime += (int)Math.Ceiling((double)p / k);
+            foreach (int p in piles)
+            {
+                totTime += (int)Math.Ceiling((double)p / mid); // use mid here, not k
             }
-            if (totalTime <= h) {
-                res = k;
-                r = k - 1;
-            } else {
-                l = k + 1;
+
+            if (totTime <= h)
+            {
+                res = mid;    // mid is a possible answer, try to go lower
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;  // too slow, need a faster eating speed
             }
         }
+
         return res;
     }
 }
+
 #Time complexity: 
 O(n∗logm)
 Space complexity: 
