9fb2fbafb424550715e621fbdd89465fe07889c1
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-04-29 11:15:51 -0700

    Update Linked List Cycle Detection.cs

--- LinkedList/Linked List Cycle Detection.cs
+++ LinkedList/Linked List Cycle Detection.cs
@@ -47,3 +47,32 @@ public class Solution {
             // return pointer;
 
 }
+
+
+//SOLUTION 2 //Time complexity: O(n) Space complexity: O(n)
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        HashSet<ListNode> seen = new HashSet<ListNode>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (seen.Contains(cur)) {
+                return true;
+            }
+            seen.Add(cur);
+            cur = cur.next;
+        }
+        return false;
+    }
+}
