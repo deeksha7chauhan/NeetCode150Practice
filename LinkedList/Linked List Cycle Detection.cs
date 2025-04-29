0beb118cb36a5ea9558370f3e228964949226d66
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-04-29 11:10:28 -0700

    Create Linked List Cycle Detection.cs

---
+++ LinkedList/Linked List Cycle Detection.cs
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool HasCycle(ListNode head) 
+{
+    if (head == null)
+        return false;
+
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast != null && fast.next != null)
+    {
+        slow = slow.next;         // move slow by 1 step
+        fast = fast.next.next;     // move fast by 2 steps
+
+        if (slow == fast)
+            return true;           // cycle found
+    }
+
+    return false;                  // fast reached end -> no cycle
+}
+//Time complexity: O(n) Space complexity: O(1)
+//After detecting a meeting point (where slow == fast):
+// Reset one pointer (slow) to the head of the list.
+// Keep the other pointer (fast) where it met slow.
+// Move both pointers one step at a time.
+// The point where they meet again → is the start of the cycle
+//ListNode pointer = head;
+
+            // while (pointer != slow)
+            // {
+            //     pointer = pointer.next;
+            //     slow = slow.next;
+            // }
+
+            // return pointer;
+
+}
